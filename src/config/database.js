5e3a93fcf13040539d718bf2eb2da4e60f04a1b5
ngokhang <ngokhang2002@gmail.com>
2022-12-24 11:19:09 +0700

    add db

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,10 @@ const connection = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection;
