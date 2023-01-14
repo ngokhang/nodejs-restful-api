6798d88070daf6999eb487f4541e7d161f6b558f
ngokhang <ngokhang2002@gmail.com>
2023-01-15 00:07:27 +0700

    add and test mongodb driver

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const { router } = require('./routes/web');
 const routerAPI = require('./routes/api');
 const fileUpload = require('express-fileupload');
 const connection = require('./config/database');
+const { MongoClient } = require('mongodb');
 
 // use express-fileupload
 app.use(fileUpload());
@@ -24,8 +25,26 @@ app.use('/', router);
 app.use('/v1/api', routerAPI);
 
 (async () => {
+  const url = process.env.DB_HOST;
+  const client = new MongoClient(url);
+  const dbName = process.env.DB_NAME;
   try {
-    await connection();
+    // await connection();
+    await client.connect();
+    console.log('Connected successfully to Server');
+    const db = client.db(dbName);
+    const collection = db.collection('customers');
+
+    // collection.insertOne({
+    //   name: 'Ngo Phu Khang',
+    //   address: 'Da Nang',
+    //   phone: '0123456789',
+    //   email: 'khangdeptry@gmail.com',
+    // });
+
+    let result = await collection.findOne({ name: 'Ngo Phu Khang' });
+    console.log(result);
+
     app.listen(port, () => {
       console.log(`Your app is listening on port ${port}`)
     });
